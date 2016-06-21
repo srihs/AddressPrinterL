bada45a5a8742089bd1cf29c45c55dd0385ad43a
srimal <srimalhs@gmail.com>
2016-06-21 06:53:21 -0700

    Customer search data retrivel

--- AddressPrinter/CustomerSearch.xaml.cs
+++ AddressPrinter/CustomerSearch.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,74 @@ namespace AddressPrinter
     /// </summary>
     public partial class CustomerSearch : MetroWindow
     {
+        DataTable dt = new DataTable();
+
         public CustomerSearch()
         {
             InitializeComponent();
+            try
+            {
+                dt.Columns.Add("Customer Name", typeof(string));
+                dt.Columns.Add("Address", typeof(string));
+                
+                dt.Columns.Add("Rep", typeof(string));
+                dgSearhCustomer.ItemsSource = dt.DefaultView;
+                txtCustomerName.Focus();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtCustomerName_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Enter)
+                {
+                    if (txtCustomerName.Text != string.Empty)
+                    {
+
+                        System.Data.SQLite.SQLiteConnection dbConnection = new Common().OpenConnection();
+                        string Sql = " Select * from Customer where Cus_CustomerName Like '%" + txtCustomerName.Text + "%'";
+                        System.Data.SQLite.SQLiteCommand dbCommand = new System.Data.SQLite.SQLiteCommand(Sql, dbConnection);
+                        dbCommand.CommandType = System.Data.CommandType.Text;
+                        System.Data.SQLite.SQLiteDataReader dReader = dbCommand.ExecuteReader();
+                        //// dbCommand.Dispose();
+                        //  dbConnection.Dispose();
+
+                        while (dReader.Read())
+                        {
+                            DataRow dRow = dt.NewRow();
+                            dRow["Customer Name"] = (dReader[1]?.ToString() ?? "").ToString();
+                            dRow["Address"] = (dReader[2]?.ToString() ?? "").ToString() + " " + (dReader[3]?.ToString() ?? "").ToString() + " "+ (dReader[4]?.ToString() ?? "").ToString();
+                            dRow["Rep"] = (dReader[8]?.ToString() ?? "").ToString();
+                            dt.Rows.Add(dRow);
+                        }
+
+                        dgSearhCustomer.ItemsSource = dt.DefaultView;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("No records found", "Address Printer", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Address Printer");
+            }
         }
     }
 }
